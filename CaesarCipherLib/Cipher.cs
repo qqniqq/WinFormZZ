d787d0b8264c10211db6a8dbd7bf9dc9241f9084
Саид Мужаидов <69said05@gmail.com>
2025-04-08 23:25:33 +0300

    Добавил шифрование кириллицы и цифр

--- CaesarCipherLib/Cipher.cs
+++ CaesarCipherLib/Cipher.cs
@@ -4,31 +4,46 @@ namespace CaesarCipher
 {
     public class Cipher
     {
+        private static readonly string Alphabet =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789";
 
         public static string Encrypt(string textToEncrypt, int key)
         {
-            if (textToEncrypt.Length == 0) return String.Empty;
+            if (string.IsNullOrEmpty(textToEncrypt))
+                return string.Empty;
 
-            char chr = textToEncrypt[0].ToString().ToLower()[0];
+            char currentChar = textToEncrypt[0];
+            int index = Alphabet.IndexOf(currentChar);
 
-            var code = IsBasicLetter(chr) ? (char)(((chr - 'a' + key) % 26) + 'a') : chr;
-            return code + Encrypt(textToEncrypt.Substring(1), key);
+            char encryptedChar = currentChar;
+
+            if (index != -1)
+            {
+                int newIndex = (index + key) % Alphabet.Length;
+                encryptedChar = Alphabet[newIndex];
+            }
+
+            return encryptedChar + Encrypt(textToEncrypt.Substring(1), key);
         }
 
         public static string Decrypt(string textToDecrypt, int key)
         {
-            if (textToDecrypt.Length == 0) return String.Empty;
+            if (string.IsNullOrEmpty(textToDecrypt))
+                return string.Empty;
 
-            char chr = textToDecrypt[0].ToString().ToLower()[0];
+            char currentChar = textToDecrypt[0];
+            int index = Alphabet.IndexOf(currentChar);
 
-            var code = IsBasicLetter(chr) ? (char)('z' - (('z' - chr + key) % 26)) : chr;
-            return code + Decrypt(textToDecrypt.Substring(1), key);
+            char decryptedChar = currentChar;
+
+            if (index != -1)
+            {
+                int newIndex = (index - key + Alphabet.Length) % Alphabet.Length;
+                decryptedChar = Alphabet[newIndex];
+            }
+
+            return decryptedChar + Decrypt(textToDecrypt.Substring(1), key);
         }
-
-        private static bool IsBasicLetter(char c)
-        {
-            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-        }
-
     }
 }
