c18433472f4a65d1eb7195a90cead78b26bc1507
Саид Мужаидов <69said05@gmail.com>
2025-04-11 22:48:04 +0300

    сделал тестирование

---
+++ CaesarCipherTests/CaesarCipherTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CaesarCipher; 
+
+namespace CaesarCipherTests
+{
+    [TestClass]
+    public class CipherTests
+    {
+        [TestMethod]
+        [DataRow("привет я Саид мне 19 лет", 2, "сткджф 1 Увкё опж 3B нжф")]
+        public void TestEncrypt(string input, int key, string expected)
+        {
+            string result = Cipher.Encrypt(input, key);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow("Jk, K'o Uckf, K'o 3B Аgctu qnf", 2, "Hi, I'm Said, I'm 19 years old")]
+        public void TestDecrypt(string input, int key, string expected)
+        {
+            string result = Cipher.Decrypt(input, key);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
